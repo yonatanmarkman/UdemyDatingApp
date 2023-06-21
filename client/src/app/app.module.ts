d31dc556ed96e2a58706b4c2bc171432e9771fae
YonatanMarkman <yonatanmarkman1@gmail.com>
2023-06-21 16:26:13 +0300

    Dropdown and Animations from ngx-bootstrap were added to the app component.

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,23 +1,26 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavComponent } from './nav/nav.component';
 import { FormsModule } from '@angular/forms';
+import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavComponent
   ],
-  imports: [
+  imports: [  
+    BrowserAnimationsModule,
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
     FormsModule,
-
+    BsDropdownModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]

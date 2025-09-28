f588842ac51fe9c224c861d481ebbdb8ed54347c
YonatanMarkman <yonatanmarkman1@gmail.com>
2025-09-28 20:22:39 +0300

    Updates to controller:
    -Set the whole controller to be under the [Authorize] attribute.
    -Updated the controller to utilize the repository interface.
    -Added method GetMemberPhotos().

--- API/Controllers/MembersController.cs
+++ API/Controllers/MembersController.cs
@@ -1,32 +1,30 @@
-using System;
-using API.Data;
 using API.Entities;
+using API.Interfaces;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.EntityFrameworkCore;
 
 namespace API.Controllers;
 
-public class MembersController(AppDbContext context) : BaseApiController
+[Authorize]
+public class MembersController(IMemberRepository memberRepository) : BaseApiController
 {
     [HttpGet] // /api/members
-    public async Task<ActionResult<IEnumerable<AppUser>>> GetMembers()
+    public async Task<ActionResult<IReadOnlyList<Member>>> GetMembers()
     {
-        List<AppUser> members = await context.Users.ToListAsync();
+        IReadOnlyList<Member> result = await memberRepository.GetMembersAsync();
 
-        return members;
+        return Ok(result);
     }
 
-    [Authorize]
     [HttpGet("{id}")] // /api/members/bob-id
-    public async Task<ActionResult<AppUser>> GetMember(string id)
+    public async Task<ActionResult<Member>> GetMember(string id)
     {
         if (string.IsNullOrWhiteSpace(id))
             return BadRequest("Member ID cannot be null or empty");
 
         try
         {
-            AppUser? member = await context.Users.FindAsync(id);
+            Member? member = await memberRepository.GetMemberByIdAsync(id);
 
             if (member == null)
                 return NotFound();
@@ -37,5 +35,14 @@ public class MembersController(AppDbContext context) : BaseApiController
         {
             return BadRequest(ex.Message);
         }
+    }
+
+    [HttpGet("{id}/photos")]
+    public async Task<ActionResult<IReadOnlyList<Photo>>> GetMemberPhotos(string id)
+    {
+        IReadOnlyList<Photo> result
+            = await memberRepository.GetPhotosForMemberAsync(id);
+
+        return Ok(result);
     }
 }

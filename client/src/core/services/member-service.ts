d552d4f040e58c1577e11509a9bac00a4cce580b
YonatanMarkman <yonatanmarkman1@gmail.com>
2025-10-09 16:58:21 +0300

    More file creations:
    -Created environement files, to remove hard-coded URL strings inside .ts files
    -Created jwt-interceptor class, in order to set the 'Authrozation: Bearer <user.token>' data in each Http request
    -Created member type in member.ts file
    -Created member-service class, which is used to obtain the member data from the backend API
    -Created member-resolver, which is used to pre-fetch the member data before accessing the page of a specific member

---
+++ client/src/core/services/member-service.ts
@@ -0,0 +1,20 @@
+import { HttpClient } from '@angular/common/http';
+import { inject, Injectable } from '@angular/core';
+import { environment } from '../../environments/environment';
+import { Member } from '../../types/member';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class MemberService {
+  private http = inject(HttpClient);
+  private baseUrl = environment.apiUrl;
+
+  getMembers() {
+    return this.http.get<Member[]>(this.baseUrl + 'members');
+  }
+
+  getMember(id: string) {
+    return this.http.get<Member>(this.baseUrl + 'members/' + id);
+  }
+}

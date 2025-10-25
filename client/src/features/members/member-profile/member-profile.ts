857b95271c0ae78c024fbe2b8c5ddba93c664000
YonatanMarkman <yonatanmarkman1@gmail.com>
2025-10-25 19:04:28 +0300

    Created an 'edit details' form in the member-profile component, with an 'Edit' button that enables it for the logged-in user.
    (As of this commit, the form does not update the details in the server side)

--- client/src/features/members/member-profile/member-profile.ts
+++ client/src/features/members/member-profile/member-profile.ts
@@ -1,21 +1,62 @@
-import { Component, inject, OnInit, signal } from '@angular/core';
+import { Component, inject, OnDestroy, OnInit, signal, ViewChild } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
-import { Member } from '../../../types/member';
+import { EditableMember, Member } from '../../../types/member';
 import { DatePipe } from '@angular/common';
+import { MemberService } from '../../../core/services/member-service';
+import { FormsModule, NgForm } from '@angular/forms';
+import { ToastService } from '../../../core/services/toast-service';
 
 @Component({
   selector: 'app-member-profile',
-  imports: [DatePipe],
+  imports: [DatePipe, FormsModule],
   templateUrl: './member-profile.html',
   styleUrl: './member-profile.css'
 })
-export class MemberProfile implements OnInit {
+export class MemberProfile implements OnInit, OnDestroy {
+  @ViewChild('editForm') editForm?: NgForm;
+
+  private toast = inject(ToastService);
   private route = inject(ActivatedRoute);
+
   protected member = signal<Member | undefined>(undefined);
+  protected memberService = inject(MemberService);
+  protected editableMember: EditableMember = {
+    displayName: '',
+    description: '',
+    city: '',
+    country: ''
+  }
+
+  constructor() {
+    
+  }
 
   ngOnInit(): void {
     this.route.parent?.data.subscribe(data => {
       this.member.set(data['member']);
     });
+    
+    this.editableMember = {
+      displayName: this.member()?.displayName || '',
+      description: this.member()?.description || '',
+      city: this.member()?.city || '',
+      country: this.member()?.country || '',
+    }
+  }
+
+  updateProfile() {
+    if (!this.member()) return;
+    // Use the spread operator, to override the member() properties
+    // with the updated editableMember's properties.
+    const updatedMember = {...this.member(), ...this.editableMember};
+    console.log(updatedMember);
+    this.toast.success('Profile updated successfully');
+    this.memberService.editMode.set(false);
+  }
+
+  ngOnDestroy(): void {
+    if (this.memberService.editMode()) {
+      this.memberService.editMode.set(false);
+    }
   }
 }

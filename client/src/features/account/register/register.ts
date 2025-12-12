357dfb535a51867173e3ea5c28fda5fcead17264
YonatanMarkman <yonatanmarkman1@gmail.com>
2025-12-12 19:18:15 +0200

    Utilizing the text-input component inside the Register Form, and re-writing the form's code to be a reactive form.
    Currently, the 'register()' method is being re-written, since we've removed the accountService,
    so I left this method empty in this commit.

--- client/src/features/account/register/register.ts
+++ client/src/features/account/register/register.ts
@@ -1,29 +1,53 @@
-import { Component, inject, Input, input, output, signal, Signal } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-import { User, RegisterCreds } from '../../../types/user';
-import { AccountService } from '../../../core/services/account-service';
+import { Component, inject, output } from '@angular/core';
+import { AbstractControl, FormBuilder, FormGroup, ReactiveFormsModule, ValidationErrors, ValidatorFn, Validators } from '@angular/forms';
+import { RegisterCreds } from '../../../types/user';
+import { JsonPipe } from '@angular/common';
+import { TextInput } from "../../../shared/text-input/text-input";
 
 @Component({
   selector: 'app-register',
-  imports: [FormsModule],
+  imports: [ReactiveFormsModule, JsonPipe, TextInput],
   templateUrl: './register.html',
   styleUrl: './register.css'
 })
 
 export class Register {
-  private accountService = inject(AccountService);
+  private formBuilder = inject(FormBuilder);
+
   cancelRegister = output<boolean>();
 
   protected creds = {} as RegisterCreds;
+  protected registerForm: FormGroup;
+
+  constructor() {
+    this.registerForm = this.formBuilder.group({
+      email: ['', 
+        [Validators.required, Validators.email]],
+      displayName: ['', 
+        [Validators.required]],
+      password: ['', 
+        [Validators.required, Validators.minLength(4), Validators.maxLength(16)]],
+      confirmPassword: ['', 
+        [Validators.required, this.matchValues('password')]]
+    });
+    this.registerForm.controls['password'].valueChanges.subscribe(() => {
+      this.registerForm.controls['confirmPassword'].updateValueAndValidity();
+    })
+  }
+
+  matchValues(matchTo: string): ValidatorFn {
+    return (control: AbstractControl): ValidationErrors | null => {
+      const parent = control.parent;
+      if (!parent)
+        return null;
+
+      const matchValue = parent.get(matchTo)?.value;
+      return control.value === matchValue ? null : {passwordMismatch: true}
+    }
+  }
 
   register() {
-    this.accountService.register(this.creds).subscribe({
-      next: response => {
-        console.log(response);
-        this.cancel();
-      },
-      error: error => console.log(error)
-    })
+    
   }
 
   cancel() {

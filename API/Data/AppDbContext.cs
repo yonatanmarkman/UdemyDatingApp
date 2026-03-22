cdf698a41cd5876e9ea3bf093bd290ebdb70bce1
YonatanMarkman <yonatanmarkman1@gmail.com>
2026-03-22 16:21:15 +0200

    Overriden the 'OnModelCreating' method in the AppDbContext, with a method that converts all universal DateTime property values into UTC DateTime values.

--- API/Data/AppDbContext.cs
+++ API/Data/AppDbContext.cs
@@ -1,5 +1,7 @@
 using API.Entities;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace API.Data;
 
@@ -8,4 +10,28 @@ public class AppDbContext(DbContextOptions options) : DbContext(options)
     public DbSet<AppUser> Users { get; set; }
     public DbSet<Member> Members { get; set; }
     public DbSet<Photo> Photos { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+
+        // Create the value converter from universal DateTime to UTC DateTime
+        var dateTimeConverter = new ValueConverter<DateTime, DateTime>(
+            value => value.ToUniversalTime(),
+            value => DateTime.SpecifyKind(value, DateTimeKind.Utc)
+        );
+
+        // For each entity type, go over each property,
+        // and utilize the converter when the property is a DateTime.
+        foreach (IMutableEntityType entityType in modelBuilder.Model.GetEntityTypes())
+        {
+            foreach (IMutableProperty property in entityType.GetProperties())
+            {
+                if (property.ClrType == typeof(DateTime))
+                {
+                    property.SetValueConverter(dateTimeConverter);
+                }
+            }
+        }
+    }
 }

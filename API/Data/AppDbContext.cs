08cd45316d7886dd49421f76ccc331d074f447ec
YonatanMarkman <yonatanmarkman1@gmail.com>
2026-04-19 15:56:42 +0300

    Updated the data model:
    - Added an entity to the ecosystem - MemberLike.
    This is the entity which is responsible for storing the "Member A likes Member B" relationship.
    - Utilized the MemberLike entity inside the Member entity class.
    - Added a DbSet<> for the MemberLike entity, inside the AppDbContext class.
    - Updated the method for model creation inside the AppDbContext class, so that the member deletion logic also correctly deletes MemberLikes as well.
    - Created the migration for all the database changes.

--- API/Data/AppDbContext.cs
+++ API/Data/AppDbContext.cs
@@ -10,10 +10,38 @@ public class AppDbContext(DbContextOptions options) : DbContext(options)
     public DbSet<AppUser> Users { get; set; }
     public DbSet<Member> Members { get; set; }
     public DbSet<Photo> Photos { get; set; }
+    public DbSet<MemberLike> Likes { get; set; }
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
         base.OnModelCreating(modelBuilder);
+
+        modelBuilder.Entity<MemberLike>()
+            .HasKey(x => new
+            {
+                x.SourceMemberId, x.TargetMemberId
+            });
+
+        // Each memberLike has one source member,
+        // and each member CAN LIKE many other members.
+        // If the source member is deleted, then all their likes are also deleted.
+        // This is set-up by the DeleteBehavior.Cascade property.
+        modelBuilder.Entity<MemberLike>()
+            .HasOne(s => s.SourceMember)
+            .WithMany(t => t.LikedMembers)
+            .HasForeignKey(s => s.SourceMemberId)
+            .OnDelete(DeleteBehavior.Cascade);
+
+        // Each memberLike has one target member,
+        // and each member CAN BE LIKED BY many other members.
+        // If the target member is deleted, their likes are unaffected.
+        // This is meant to prevent circular cascade deletions, but this also means
+        // that we must handle the 'orphaned like records' by ourselves, manually.
+        modelBuilder.Entity<MemberLike>()
+            .HasOne(s => s.TargetMember)
+            .WithMany(t => t.LikedByMembers)
+            .HasForeignKey(s => s.TargetMemberId)
+            .OnDelete(DeleteBehavior.NoAction);
 
         // Create the value converter from universal DateTime to UTC DateTime
         var dateTimeConverter = new ValueConverter<DateTime, DateTime>(
--- API/Entities/Member.cs
+++ API/Entities/Member.cs
@@ -26,6 +26,12 @@ public class Member
     public List<Photo> Photos { get; set; } = [];
 
     [JsonIgnore]
+    public List<MemberLike> LikedByMembers { get; set; } = [];
+    
+    [JsonIgnore]
+    public List<MemberLike> LikedMembers { get; set; } = [];
+
+    [JsonIgnore]
     [ForeignKey(nameof(Id))]
     public AppUser User { get; set; } = null!;
 }
